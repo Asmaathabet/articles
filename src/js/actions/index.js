d26ce6f81d610634eaff13b18ec001ee11fe434c
Asmaathabet <asmathabet2009@hotmail.com>
2020-09-05 13:56:16 +0300

    convert strings to constants file to avoid typos and errors

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,5 +1,7 @@
+import { ADD_ARTICLE } from '../constants/action-types';
+
 const AddArticle = (payload) => {
-    return {type: 'ADDARTICLE', payload }
+    return {type: ADD_ARTICLE , payload }
 }
 
 export default AddArticle; 
