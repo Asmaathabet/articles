b3995b480659ea40fd23ca9f5a6546015ef1c96a
Asmaathabet <asmathabet2009@hotmail.com>
2020-09-05 17:50:59 +0300

    create middleware for unlegal words

---
+++ src/js/middlewares/index.js
@@ -0,0 +1,22 @@
+import {ADD_ARTICLE} from '../constants';
+
+const forbiddenWords = ["spam", "government", "flag"]; 
+
+const forbiddenWordsMiddleware = ({ dispatch }) => {
+    return (next)=> {
+      return (action)=> {
+        if (action.type === ADD_ARTICLE) {
+            const foundWord = forbiddenWords.filter(word =>
+              action.payload.title.includes(word)
+            );
+    
+            if (foundWord.length) {
+              return dispatch({ type: "FOUND_BAD_WORD" });
+            }
+          }
+        return next(action);
+      }
+    }
+  }
+
+  export default forbiddenWordsMiddleware;
