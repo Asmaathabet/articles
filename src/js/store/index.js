3dd9b0c199eede5046cb2559ea13cc73c7cc35e0
Asmaathabet <asmathabet2009@hotmail.com>
2020-09-05 17:55:43 +0300

    apply middleware by adding it to the store:

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,8 +1,11 @@
-import { createStore } from "redux"; 
+import { createStore, applyMiddleware } from "redux"; 
 import rootReducer from '../reducers/index.js';
+import forbiddenWordsMiddleware from '../middlewares';
 
-
-const store = createStore(rootReducer);
+const store = createStore(
+    rootReducer, 
+    applyMiddleware(forbiddenWordsMiddleware)
+    );
 
 export default store; 
 
