ed1527c3e9d95506e1b5d0b3be3b0626c06a07ce
Asmaathabet <asmathabet2009@hotmail.com>
2020-09-05 15:56:04 +0300

    - connect redux with react by installing react-redux library - delete unneeded files and prepare index file to work with redux - create components folder that contain App file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 
-import index from './js';
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './js/store';
+import App from './components/App';
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+); 
+
+
+
 
